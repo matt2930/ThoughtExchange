e5c5a899490f07139915baba1538efc7e4f82259
root <root@TRTOT4901908EX1.jsq.bsg.ad.adp.com>
2021-04-08 00:01:14 -0400

    Made some progress, decided to use RabbitMQ image for message queue. For some reason, flask app is not connecting to the channel and publishing.

--- fib-calc/fib.py
+++ fib-calc/fib.py
@@ -2,13 +2,42 @@ import pika
 import os, sys
 from time import sleep
 
-fpid = os.fork()
-if fpid != 0:
-    sys.exit(0)
+# fpid = os.fork()
+# if fpid != 0:
+#     sys.exit(0)
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='rpc_queue')
+def daemonize():
+    """UNIX double fork mechanism."""
+    try:
+        pid = os.fork()
+        if pid > 0:
+            # exit first parent
+            sys.exit(0)
+    except OSError as err:
+        sys.stderr.write('_Fork #1 failed: {0}\n'.format(err))
+        sys.exit(1)
+    # decouple from parent environment
+    os.chdir('/')
+    os.setsid()
+    os.umask(0)
+    # do second fork
+    try:
+        pid = os.fork()
+        if pid > 0:
+            # exit from second parent
+            sys.exit(0)
+    except OSError as err:
+        sys.stderr.write('_Fork #2 failed: {0}\n'.format(err))
+        sys.exit(1)
+    # redirect standard file descriptors
+    sys.stdout.flush()
+    sys.stderr.flush()
+    si = open(os.devnull, 'r')
+    so = open(os.devnull, 'w')
+    se = open(os.devnull, 'w')
+    os.dup2(si.fileno(), sys.stdin.fileno())
+    os.dup2(so.fileno(), sys.stdout.fileno())
+    os.dup2(se.fileno(), sys.stderr.fileno())
 
 def fib(n):
     a = 0
@@ -32,8 +61,21 @@ def on_request(ch, method, props, body):
                      body=str(response))
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
+connected = False
+while not connected:
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters('messagequeue'))
+        connected = True
+    except:
+        print("Could not connect")
+        sleep(5)
+channel = connection.channel()
+channel.queue_declare(queue='rpc_queue')
+
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='rpc_queue',on_message_callback=on_request)
 
 print(" [x] Awaiting RPC requests")
 channel.start_consuming()
+
+daemonize()
--- web-app/app/app.py
+++ web-app/app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import pika
 
 app = Flask(__name__)
 
@@ -12,9 +13,16 @@ def index():
 @app.route('/fib')
 def index2():
     num = request.form['number']
-    # connection = pika.BlockingConnection(pika.ConnectionParameters('message-queue'))
-    # channel = connection.channel
-    return '<h1>Test %s </h1>' % num
+    connected = False
+    while not connected:
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters('messagequeue'))
+            connected = True
+        except:
+            print("Could not connect")
+    channel = connection.channel()
+
+    channel.basic_publish(routing_key='rpc_queue', body=str(num))
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
